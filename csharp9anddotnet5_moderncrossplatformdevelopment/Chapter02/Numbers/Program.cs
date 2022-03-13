a1a4b51a7087efaa24304ad5a053c92a9aa07aa9
Zhixin He <zhixin.he@hotmail.com>
2022-03-13 10:00:53 -0700

    add .gitignore

--- csharp9anddotnet5_moderncrossplatformdevelopment/Chapter02/Numbers/Program.cs
+++ csharp9anddotnet5_moderncrossplatformdevelopment/Chapter02/Numbers/Program.cs
@@ -17,3 +17,15 @@ Console.WriteLine($"{nameof(naturalNumber)} = {naturalNumber}");
 Console.WriteLine($"{nameof(integerNumber)} = {integerNumber}");
 Console.WriteLine($"{nameof(realNumber)} = {realNumber}");
 Console.WriteLine($"{nameof(anotherRealNumber)} = {anotherRealNumber}");
+
+Console.WriteLine($"sbyte uses {sizeof(sbyte)} bytes and can store numbers in the range {sbyte.MinValue:N0} to {sbyte.MaxValue:N0}");
+Console.WriteLine($"byte uses {sizeof(byte)} bytes and can store numbers in the range {byte.MinValue:N0} to {byte.MaxValue:N0}");
+Console.WriteLine($"short uses {sizeof(short)} bytes and can store numbers in the range {short.MinValue:N0} to {short.MaxValue:N0}");
+Console.WriteLine($"ushort uses {sizeof(ushort)} bytes and can store numbers in the range {ushort.MinValue:N0} to {ushort.MaxValue:N0}");
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+Console.WriteLine($"uint uses {sizeof(uint)} bytes and can store numbers in the range {uint.MinValue:N0} to {uint.MaxValue:N0}");
+Console.WriteLine($"long uses {sizeof(long)} bytes and can store numbers in the range {long.MinValue:N0} to {long.MaxValue:N0}");
+Console.WriteLine($"ulong uses {sizeof(ulong)} bytes and can store numbers in the range {ulong.MinValue:N0} to {ulong.MaxValue:N0}");
+Console.WriteLine($"float uses {sizeof(float)} bytes and can store numbers in the range {float.MinValue:E} to {float.MaxValue:E}");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:E} to {double.MaxValue:E}");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
---
+++ csharp9anddotnet5_moderncrossplatformdevelopment/Chapter02/ioformat/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Type your first name and press ENTER: ");
+string firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine();
+
+Console.WriteLine(
+    format: "{0:-10} {1:6}",
+    arg0: "First Name",
+    arg1: "Age"
+);
+Console.WriteLine(
+    format: "{0:-10} {1:6}",
+    arg0: firstName,
+    arg1: age
+);
